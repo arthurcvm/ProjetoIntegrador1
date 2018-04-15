5604ccf6adf966c44a4b9bf69fe2d6320ac9c1d2
csarp <csarp@DESKTOP-3TVLSSK.local>
2018-04-15 19:37:53 -0300

    atualização das entidades

--- ProjetoIntegrador1/src/model/Pessoa.java
+++ ProjetoIntegrador1/src/model/Pessoa.java
@@ -10,13 +10,8 @@ package model;
  * @author csarp
  */
 public class Pessoa {
-    private String nome;
-    private String cpf;
-
-    public Pessoa(String nome, String cpf) {
-        this.nome = nome;
-        this.cpf = cpf;
-    }
+    protected String nome;
+    protected String cpf;
 
     public String getNome() {
         return nome;
